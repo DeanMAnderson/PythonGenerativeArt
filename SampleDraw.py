c8bda99d8b003cbac31f8118634185dfc6a66b7d
Dean Anderson <DeanMAnderson@hotmail.co.uk>
2019-01-09 20:38:30 +0000

    Add SampleDraw
    Basic drawing commands using turtle module

---
+++ SampleDraw.py
@@ -0,0 +1,10 @@
+import turtle
+
+window = turtle.Screen();
+
+turtlePen = turtle.Turtle();
+turtlePen.shape("turtle");
+turtlePen.forward(50);
+turtlePen.right(90);
+turtlePen.forward(50);
+turtle.done();
